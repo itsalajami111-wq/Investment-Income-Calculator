03c37b590286f5909a2fb474f926331fecf0babc
itsalajami111-wq <itsalajami111@gmail.com>
2026-03-13 07:07:20 +0400

    Update lead.js

--- api/lead.js
+++ api/lead.js
@@ -21,9 +21,6 @@ export default async function handler(req, res) {
             "str:cm:country": data.country || "",
             "str:cm:answers": "Investment Income Calculator"
           },
-          fields: {
-            "str::email": data.email || ""
-          }
         }
       ],
       merge_by: ["str::email"]
