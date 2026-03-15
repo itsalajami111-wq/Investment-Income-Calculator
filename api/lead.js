63a19f8aedc9535616749849eb1ac250a8e95b3d
itsalajami111-wq <itsalajami111@gmail.com>
2026-03-16 00:47:14 +0400

    Update lead.js

--- api/lead.js
+++ api/lead.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
     const orttoBody = {
       activities: [
         {
-          activity_id: "act:cm:investment-calculator-form-submitted",
+          activity_id: "website_form_submit",
           attributes: {
             "phn:cm:phone-number": {
               c: phoneParsed.countryCodeDigits,
